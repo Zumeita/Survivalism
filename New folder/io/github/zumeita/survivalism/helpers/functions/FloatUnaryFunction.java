b60b4238e61588ba42544c958e0ba4fdfadb5ebe
Zumeita <Zetrx>
2022-04-13 01:44:45 +0100

    Soil/Earth types implemented. (Humus, Topsoil, Subsoil) First revision of custom surface gen, layering the new soil types. Connected Grass! Spreading still needs working on.
    Signed-off-by: Zumeita <Zetrx>

--- New folder/io/github/zumeita/survivalism/helpers/functions/FloatUnaryFunction.java
+++
@@ -1,7 +0,0 @@
-package io.github.zumeita.survivalism.helpers.functions;
-
-@FunctionalInterface
-public interface FloatUnaryFunction
-{
-    float applyAsFloat(float f);
-}
--- New folder/io/github/zumeita/survivalism/registration/BiomeRegistration.java
+++
@@ -1,8 +0,0 @@
-package io.github.zumeita.survivalism.registration;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-public class BiomeRegistration {
-
-}
